f057e0ae247303d63e094901343214fa833736a8
rq-rafsanjani <74598357+rq-rafsanjani@users.noreply.github.com>
2020-11-17 12:14:18 +0100

    Update FixedWidthToDelimiter.py

--- FixedWidthToDelimiter.py
+++ FixedWidthToDelimiter.py
@@ -5,15 +5,18 @@ Sample run: (Order of argument doesnt matter)
 python3 FixedWidthToDelimiter.py -i SrcFile.txt -o TrgFile.csv -c Config.txt -d ","
 
 Inputs are as follows
-1. Input FIle - Mandatory(Argument -i) - File which has fixed Width data in it
+1. Input File - Mandatory(Argument -i) - File which has fixed Width data in it
+    use .dst or .pdb files generated from the chemo/bioinformatics program
 2. Config File - Optional (Argument -c, if not provided will look for Config.txt file on same path, if not present script will not run)
-    Should have format as
+    For each column, format as following, example for dividing .dst file into columns
     FieldName,fieldLength
     eg:
-    FirstName,10
-    SecondName,8
-    Address,30
-    etc:
+    x1,6
+    x2,5
+    x3,16
+    x4,32
+    x5,12
+    x6,8
 3. Output File - Optional (Argument -o, if not provided will be used as InputFIleName plus Delimited.txt)
 4. Delimiter - Optional (Argument -d, if not provided default value is "|" (pipe))
 
